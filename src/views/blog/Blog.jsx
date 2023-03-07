3933289719b11ed0edb34b12593171fc96aee40f
Zaide Kurti <zaiido1920@gmail.com>
2023-03-07 16:02:53 +0100

    Download file button

--- src/views/blog/Blog.jsx
+++ src/views/blog/Blog.jsx
@@ -1,6 +1,7 @@
 import { format, parseISO } from "date-fns";
 import React, { useEffect, useState } from "react";
 import { Button, Container, Form, Image, ListGroup, Modal } from "react-bootstrap";
+import { Download } from "react-bootstrap-icons";
 import { useParams } from "react-router-dom";
 import BlogAuthor from "../../components/blog/blog-author/BlogAuthor";
 import BlogLike from "../../components/likes/BlogLike";
@@ -136,6 +137,7 @@ const Blog = (props) => {
     }
   }
 
+
   useEffect(() => {
     const { id } = params;
     getBlog(id)
@@ -154,82 +156,85 @@ const Blog = (props) => {
     return <div>loading</div>;
   } else {
     return (
-      <> {blog && <div className="blog-details-root">
-        <Container>
-          <Image className="blog-details-cover" src={blog.cover} fluid />
+      <>
+        {blog && <div className="blog-details-root">
+          <Container>
+            <Image className="blog-details-cover" src={blog.cover} fluid />
 
-          <Form.Group className="d-flex">
-            <Form.Control type="file" label="Example file input"
-              onChange={(e) => {
-                const files = e.target.files
-                if (files && files.length > 0) {
-                  setFile(files[0]);
-                } else {
-                  setFile(null);
+            <Form.Group className="d-flex justify-content-center">
+              <Form.Control type="file" style={{ maxWidth: "85%" }}
+                onChange={(e) => {
+                  const files = e.target.files
+                  if (files && files.length > 0) {
+                    setFile(files[0]);
+                  } else {
+                    setFile(null);
+                  }
+                }} />
+              <Button variant="primary" style={{ marginLeft: "5px" }} onClick={(e) => {
+                e.preventDefault()
+                if (file) {
+                  handleImageUpload(blog._id)
                 }
-              }} />
-            <Button variant="primary" style={{ marginLeft: "5px" }} onClick={(e) => {
-              e.preventDefault()
-              if (file) {
-                handleImageUpload(blog._id)
-              }
-            }}>Update</Button>
-          </Form.Group>
+              }}>Update</Button>
+              <a href={`${process.env.REACT_APP_BE_URL}/blogPosts/${blog._id}/pdf`} className="btn btn-primary" style={{ marginLeft: "5px" }}><Download /> PDF</a>
+            </Form.Group>
 
-          <h1 className="blog-details-title">{blog.title}</h1>
 
-          <div className="blog-details-container">
-            <div className="blog-details-author">
-              <BlogAuthor {...blog.author} />
-            </div>
-            <div className="blog-details-info">
-              <div>{format(parseISO(blog.createdAt.toString()), 'dd MMM yyyy')}</div>
-              <div>{`${blog.readTime.value} ${blog.readTime.unit} read`}</div>
-              <div
-                style={{
-                  marginTop: 20,
-                }}
-              >
-                <BlogLike defaultLikes={["123"]} />
+            <h1 className="blog-details-title">{blog.title}</h1>
+
+            <div className="blog-details-container">
+              <div className="blog-details-author">
+                <BlogAuthor {...blog.author} />
+              </div>
+              <div className="blog-details-info">
+                <div>{format(parseISO(blog.createdAt.toString()), 'dd MMM yyyy')}</div>
+                <div>{`${blog.readTime.value} ${blog.readTime.unit} read`}</div>
+                <div
+                  style={{
+                    marginTop: 20,
+                  }}
+                >
+                  <BlogLike defaultLikes={["123"]} />
+                </div>
               </div>
             </div>
-          </div>
 
-          <div
-            dangerouslySetInnerHTML={{
-              __html: blog.content,
-            }}
-          ></div>
-          {comments && <ListGroup>
-            {comments.map((comment) => {
-              return <ListGroup.Item key={comment._id}>
-                <div className="d-flex">
-                  <div className="d-flex flex-column" style={{ marginRight: "auto" }}>
-                    <div>{comment.comment}</div>
-                    <div style={{ fontSize: "14px" }}>- {comment.author}</div>
-                  </div>
+            <div
+              dangerouslySetInnerHTML={{
+                __html: blog.content,
+              }}
+            ></div>
+            {comments && <ListGroup>
+              {comments.map((comment) => {
+                return <ListGroup.Item key={comment._id}>
                   <div className="d-flex">
-                    <Button className="mt-2" style={{ marginRight: "5px" }} variant="outline-primary" onClick={() => { handleShow(comment._id) }}>Edit</Button>
-                    <Button className="mt-2" variant="danger" onClick={(e) => {
-                      e.preventDefault()
-                      handleDelete(blog._id, comment._id)
-                    }}>Delete</Button>
-                  </div>
+                    <div className="d-flex flex-column" style={{ marginRight: "auto" }}>
+                      <div>{comment.comment}</div>
+                      <div style={{ fontSize: "14px" }}>- {comment.author}</div>
+                    </div>
+                    <div className="d-flex">
+                      <Button className="mt-2" style={{ marginRight: "5px" }} variant="outline-primary" onClick={() => { handleShow(comment._id) }}>Edit</Button>
+                      <Button className="mt-2" variant="danger" onClick={(e) => {
+                        e.preventDefault()
+                        handleDelete(blog._id, comment._id)
+                      }}>Delete</Button>
+                    </div>
 
-                </div>
-              </ListGroup.Item>
-            })}
-          </ListGroup>}
-          <Form.Group controlId="blog-form" className="mt-3">
-            <Form.Control className="mt-2" size="lg" placeholder="Add a comment" value={commentToPost} onChange={(e) => setCommentToPost(e.target.value)} />
-            <Form.Control className="mt-2" size="lg" placeholder="Your name" value={author} onChange={(e) => setAuthor(e.target.value)} />
-            <Button className="mt-2" variant="primary" onClick={(e) => {
-              e.preventDefault()
-              handleSubmit(blog._id)
-            }}>Submit</Button>
-          </Form.Group>
-        </Container>
-      </div>}
+                  </div>
+                </ListGroup.Item>
+              })}
+            </ListGroup>}
+            <Form.Group controlId="blog-form" className="mt-3">
+              <Form.Control className="mt-2" size="lg" placeholder="Add a comment" value={commentToPost} onChange={(e) => setCommentToPost(e.target.value)} />
+              <Form.Control className="mt-2" size="lg" placeholder="Your name" value={author} onChange={(e) => setAuthor(e.target.value)} />
+              <Button className="mt-2" variant="primary" onClick={(e) => {
+                e.preventDefault()
+                handleSubmit(blog._id)
+              }}>Submit</Button>
+            </Form.Group>
+          </Container>
+        </div>}
         <Modal show={show} onHide={handleClose}>
           <Modal.Header closeButton>
             <Modal.Title>Modal heading</Modal.Title>
